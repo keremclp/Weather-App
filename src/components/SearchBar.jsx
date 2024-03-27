afd24d400c465c0527378006d33f4a3a2635be2a
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-03-27 17:03:24 +0300

    add: fetching data properly

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 /*import Logo.svg from public folder*/
 import Logo from "../Vector.svg";
-function SearchBar() {
+import { useState } from "react";
+function SearchBar({ onCitySelect }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  // const apiKey = process.env.WEATHER_APP_KEY; check the using of env variables
+  const handleInputChange = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${term}&appid=ca248cee0c1175401424a91fab6b1b59`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setSearchResults(data.list);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  const handleSelectCity = (city) => {
+    onCitySelect(city);
+    setSearchTerm("");
+    setSearchResults([]);
+  };
+
   return (
     /*Use logo.svg*/
     <>
@@ -22,7 +48,12 @@ function SearchBar() {
           </div>
         </div>
         <div className="text-field">
-          <input type="text" placeholder="Search a location" />
+          <input
+            type="text"
+            placeholder="Search a location"
+            value={searchTerm}
+            onChange={handleInputChange}
+          />
         </div>
       </div>
     </>
