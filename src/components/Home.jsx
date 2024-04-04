93da1adef9e773504e5924dfa458fbb454925385
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-04-04 15:15:44 +0300

    fix: height issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,22 +11,31 @@ const Home = ({ currentWeather, forecast }) => {
 
   // Render your Home component with the selected result
   return (
-    <div className="flex flex-col justify-center content-center bg-gray-900 rounded-lg ">
-      <div className="p-2 bg-gray-800 rounded-md m-1">
-        <div className="card">
-          <div className="current-weather w-[335px] h-[304px]">
-            {currentWeather && <CurrentWeather data={currentWeather} />}
+    <>
+      <div className="flex flex-col justify-center content-center bg-gray-900 rounded-lg ">
+        <div className="p-2 bg-gray-800 rounded-md m-1">
+          <div className="card">
+            <div className="current-weather w-[335px] h-[304px]">
+              {currentWeather && <CurrentWeather data={currentWeather} />}
+            </div>
+          </div>
+        </div>
+        <div className="p-2 mt-1 bg-gray-800 rounded-md m-1">
+          <div className="card">
+            <div className="w-full h-[284px] ">
+              <WeatherDetail data={currentWeather} />
+            </div>
+          </div>
+        </div>
+        <div className="p-2 mt-1 bg-gray-800 rounded-md m-1">
+          <div className="card">
+            <div className="w-full h-[176px] ">
+              <Forecast data={forecast} />
+            </div>
           </div>
         </div>
       </div>
-      <div className="p-2 mt-1 bg-gray-800 rounded-md m-1">
-        <div className="card">
-          <div className="w-full h-[284px] ">
-            <WeatherDetail data={currentWeather} />
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
