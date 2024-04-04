ec3c3a0ecbd2ee5cef3888601becaf48b959cd0a
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-04-04 15:50:41 +0300

    add: forecast properties

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 
+const WEEK_DAYS = ["Mon", "Tue", "Wed", "Thu", "Sun"];
 function Forecast({ data }) {
+  console.log("forecast:", data);
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek + 1).concat(
+    WEEK_DAYS.slice(0, dayInAWeek + 1)
+  );
+
   return (
-    <div>
-      {data.list.splice(0, 7).map((item, idx) => (
-        <div key={idx}>
-          <h1>{item.dt_txt}</h1>
-          <h2>{item.weather[0].description}</h2>
-          <h2>{item.main.temp}°C</h2>
+    <>
+      <div className="relative">
+        <div className="absolute w-[335px] h-[152px] top-3 flex flex-row">
+          {data.list.splice(0, 5).map((item, idx) => (
+            <div className="w-[67px] h-[152px] gap-1 ml-1">
+              <p className="text-white"> {forecastDays[idx]} </p>
+              <img
+                src={`icons/${item.weather[0].icon}.png`}
+                className="w-14 mt-1"
+                alt="weather"
+              />
+              <p className="font-nunito font-bold text-xs text-white mt-6">
+                {item.main.temp}°C
+              </p>
+              <p className="font-nunito font-normal text-xs text-gray-400">
+                {data.list[0].main.temp}
+              </p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 }
 
